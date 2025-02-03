2bbd97744a7d99dfc47c44d7ab38cca01a69bc50
CadeM01 <cadenmaki1@gmail.com>
2025-02-02 22:54:45 -0800

    changed tip to 15%

--- TaskB.java
+++ TaskB.java
@@ -12,7 +12,18 @@ public class TaskB
     public static void main (String[]arg)
     {
 
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many people are in your party?: ");
+        int partySize = in.nextInt();//takes user input for party size
+        System.out.println();
 
+        System.out.print("What is the total bill amount before tax and tip?: ");
+        double billAmount = in.nextDouble();//takes user input for bill amount
+        System.out.println();
+
+        //declares variables for tax and tip rate
+        double tax = .0725;
+        double tip = .20;
 
         //Processing
         double tipAmount = (billAmount * tip);
